2a4a4de4073af6f0359c9721addc948c22014575
Fraene <fraene7@gmail.com>
2024-12-23 16:15:24 +0100

    url fix

--- src/environment/environment.prod.ts
+++ src/environment/environment.prod.ts
@@ -1,5 +1,5 @@
 export const API = {
-	url: `https://https://strapi.portfolio.fraene.dev`
+	url: `https://strapi.portfolio.fraene.dev`
 };
 
 export const captchaSiteKey = '6LfATJ4qAAAAAMBMmzq7M0MsM1jJxVB48zBO3U1a';
